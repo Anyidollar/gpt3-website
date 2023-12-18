1540997c0a32106bea623dfcfba123867c7f902d
Anyidollar <ifeanyionyekwere247@gmail.com>
2023-12-18 02:22:38 +0100

    commited

--- my-gpt3-website/src/App.js
+++ my-gpt3-website/src/App.js
@@ -1,17 +1,29 @@
-import React from 'react'
-import {CTA, Feature, Navbar,Article,Brand} from "./components";
-
+import React from "react";
+import { CTA, Feature, Navbar, Article, Brand } from "./components";
+import {
+  Blog,
+  Features,
+  Footer,
+  Header,
+  Possibility,
+  WhatGPT3,
+} from "./containers";
 
 const App = () => {
   return (
-    <div className='App'>
-      <div className='gradient__bg'>
+    <div className="App">
+      <div className="gradient__bg">
         <Navbar />
-
+        <Header />
       </div>
-
+      <Brand />
+      <WhatGPT3 />
+      <Features />
+      <Blog />
+      <Possibility />
+      <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
