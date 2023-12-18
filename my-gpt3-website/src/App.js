1bd0670a0461236d319f9d9d667b2da25a7f9c57
Anyidollar <ifeanyionyekwere247@gmail.com>
2023-12-18 02:04:54 +0100

    commited

--- my-gpt3-website/src/App.js
+++ my-gpt3-website/src/App.js
@@ -1,9 +1,15 @@
 import React from 'react'
+import {CTA, Feature, Navbar,Article,Brand} from "./components";
+
 
 const App = () => {
   return (
-    <div>
-        <h1>Ifeanyi...</h1>
+    <div className='App'>
+      <div className='gradient__bg'>
+        <Navbar />
+
+      </div>
+
     </div>
   )
 }
